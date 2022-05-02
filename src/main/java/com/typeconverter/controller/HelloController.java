8df786e445fa59b0ed907bc7d27d816b211cc8af
전민우 <mw.jeon@lifesemantics.kr>
2022-05-02 15:42:39 +0900

    Initial commit

---
+++ src/main/java/com/typeconverter/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/v1/test")
+    public Integer test1(HttpServletRequest request) {
+        String data = request.getParameter("data"); // 모두 문자로만 나옴
+        Integer integer = Integer.valueOf(data);
+        return integer;
+    }
+
+    @GetMapping("/v2/test")
+    public String test2(@RequestParam Integer data) { // 넘어온 문자를 Integer로 형변환한 것
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+}
